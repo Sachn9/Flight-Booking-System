729622c3abb834290157cbeb98c335a550b0ebf8
Sachin kumar <sachinkumar@Sachins-MacBook-Air.local>
2025-09-09 17:20:26 +0530

    complet- aircraftApi

--- Aircraft-API/src/main/java/com/Flight_Booking_System/Aircraft_API/controler/AircraftController.java
+++ Aircraft-API/src/main/java/com/Flight_Booking_System/Aircraft_API/controler/AircraftController.java
@@ -1,28 +1,70 @@
 package com.Flight_Booking_System.Aircraft_API.controler;
 
+import com.Flight_Booking_System.Aircraft_API.connector.AirLineApiConnector;
+import com.Flight_Booking_System.Aircraft_API.connector.FlightApiConnector;
 import com.Flight_Booking_System.Aircraft_API.dto.AircraftDetailsDTO;
+import com.Flight_Booking_System.Aircraft_API.exception.AircraftDoesNotExist;
+import com.Flight_Booking_System.Aircraft_API.exception.InvalidOperationException;
 import com.Flight_Booking_System.Aircraft_API.model.Aircraft;
+import com.Flight_Booking_System.Aircraft_API.model.Airline;
+import com.Flight_Booking_System.Aircraft_API.model.Flight;
 import com.Flight_Booking_System.Aircraft_API.service.AircraftService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/aircraft")
 public class AircraftController {
 
     AircraftService aircraftService;
+    AirLineApiConnector airLineApiConnector;
+    FlightApiConnector flightApiConnector;
     @Autowired
-    public AircraftController(AircraftService aircraftService){
+    public AircraftController(AircraftService aircraftService,
+                              AirLineApiConnector airLineApiConnector,
+                              FlightApiConnector flightApiConnector){
         this.aircraftService=aircraftService;
+        this.airLineApiConnector=airLineApiConnector;
+        this.flightApiConnector=flightApiConnector;
     }
 
     @PostMapping("/register")
-    public Aircraft aircraftRegistration(@RequestBody AircraftDetailsDTO aircraftDetailsDTO){
-        Aircraft airCraft=aircraftService.airCraftRegistration(aircraftDetailsDTO);
-        return airCraft;
+    public Aircraft aircraftRegistration(@RequestBody AircraftDetailsDTO aircraftDetailsDTO,
+                                         @RequestParam UUID airlineID,
+                                         @RequestParam List<UUID> flightID){
+        Airline airline=airLineApiConnector.getAirlineDetails(airlineID);
+        List<Flight> flight=flightApiConnector.getFlightDetails(flightID);
+        return  aircraftService.airCraftRegistration(aircraftDetailsDTO,airline,flight);
 
+    }
+
+    @GetMapping("/get/airCraftById/{aircraftId}")
+    public ResponseEntity getAirCraftById(@PathVariable UUID aircraftId){
+
+        try{
+            Aircraft aircraft=aircraftService.getAirCraftsById(aircraftId);
+            return new ResponseEntity<>(aircraft, HttpStatus.OK);
+        }catch (InvalidOperationException invalidOperationException){
+            return new ResponseEntity<>(invalidOperationException.getMessage(),HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @GetMapping("get/all/aircraft")
+    public ResponseEntity getAllAirCrafts() {
+        try {
+            List<Aircraft> aircraftList = aircraftService.getAllAirCrafts();
+            return new ResponseEntity<>(aircraftList, HttpStatus.OK);
+
+        } catch (AircraftDoesNotExist aircraftDoesNotExist) {
+            return new ResponseEntity<>(aircraftDoesNotExist.getMessage(), HttpStatus.NOT_FOUND);
+        }catch (Exception e) {
+            return new ResponseEntity<>("An unexpected error occurred: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- Aircraft-API/src/main/java/com/Flight_Booking_System/Aircraft_API/service/AircraftService.java
+++ Aircraft-API/src/main/java/com/Flight_Booking_System/Aircraft_API/service/AircraftService.java
@@ -1,22 +1,51 @@
 package com.Flight_Booking_System.Aircraft_API.service;
 
+import com.Flight_Booking_System.Aircraft_API.connector.DataBaseApiConnector;
 import com.Flight_Booking_System.Aircraft_API.dto.AircraftDetailsDTO;
+import com.Flight_Booking_System.Aircraft_API.exception.InvalidOperationException;
 import com.Flight_Booking_System.Aircraft_API.model.Aircraft;
 import com.Flight_Booking_System.Aircraft_API.model.Airline;
+import com.Flight_Booking_System.Aircraft_API.model.Flight;
 import com.Flight_Booking_System.Aircraft_API.utility.MappingUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class AircraftService {
 
     MappingUtility mappingUtility;
+    DataBaseApiConnector dataBaseApiConnector;
     @Autowired
-    public AircraftService(MappingUtility mappingUtility){
+    public AircraftService(MappingUtility mappingUtility,
+                           DataBaseApiConnector dataBaseApiConnector){
         this.mappingUtility=mappingUtility;
+        this.dataBaseApiConnector=dataBaseApiConnector;
     }
 
-    public Aircraft airCraftRegistration(AircraftDetailsDTO aircraftDetailsDTO){
-        Aircraft aircraft=mappingUtility.mapAircraftToAircraftDetailsDTO(aircraftDetailsDTO, Airline.)
+    public Aircraft airCraftRegistration(AircraftDetailsDTO aircraftDetailsDTO, Airline airline, List<Flight> flight){
+        Aircraft aircraft=mappingUtility.mapAircraftToAircraftDetailsDTO(aircraftDetailsDTO, airline,flight);
+        return dataBaseApiConnector.callGetCreateAirCraftEndPoint(aircraft);
+
+    }
+
+    public Aircraft getAirCraftsById(UUID aircraftId){
+        Aircraft aircraft=dataBaseApiConnector.callGetAirCraftById(aircraftId);
+        if(aircraft ==  null){
+            throw new InvalidOperationException(String.format("Aircraft Id does not exist in database",aircraftId.toString()));
+        }
+        return aircraft;
+    }
+
+    public List<Aircraft> getAllAirCrafts() {
+        List<Aircraft> aircraftList = dataBaseApiConnector.callGetAllAirCraftsEndPoint();
+
+        if (aircraftList == null || aircraftList.isEmpty()) {
+            throw new InvalidOperationException("No AirCrafts found in the database");
+        }
+
+        return aircraftList;
     }
 }
